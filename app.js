b3660450a4ca60e9d41f22c09d97a62e87ef2c2b
Hawi-Demoz <hawipeter1@gmail.com>
2025-09-30 23:51:14 +0300

    commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('server running at localhost:3000')
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome to the subscription tracker API');
+});
+
+app.listen(3000, () => {
+    console.log('Subscription tracker API is running on http://localhost:3000');
+});
+export default app;
